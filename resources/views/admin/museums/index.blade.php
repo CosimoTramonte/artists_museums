982d36de4b945698f5dafa8e0ac0cbfa75c3535b
CosimoTramonte <cosimotramonte7@gmail.com>
2023-06-20 17:42:34 +0200

    add index of CRUD of museums

---
+++ resources/views/admin/museums/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container p-5">
+    <h1 class="fs-4 my-4">
+        Museums List
+    </h1>
+
+    @if (session('deleted'))
+        <div class="alert alert-success" role="alert">
+            {{ session('deleted') }}
+        </div>
+    @endif
+
+    <table class="table table-dark table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">City</th>
+            <th scope="col">Address</th>
+            <th scope="col">Open hour</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($museums as $museum)
+                <tr>
+                    <th scope="row">{{$museum->id}}</th>
+                    <td>{{$museum->name}}</td>
+                    <td>{{$museum->city}}</td>
+                    <td>{{$museum->address}}</td>
+                    <td>{{ $museum->open_hour }}</td>
+                    <td>
+                        <a href="#" class="btn btn-success">SHOW</a>
+                        <a href="#" class="btn btn-warning">EDIT</a>
+
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</div>
+@endsection
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -18,6 +18,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.museums.index')}}">List Museums</a>
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
